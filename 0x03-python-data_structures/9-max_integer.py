8e4143aa6858e5a4d88fb4cc688b9d170acfd92a
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-12-06 11:27:17 +0000

    task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    max_num = my_list[0]
     for num in my_list:
-        if not my_list:
-            return None
-        elif num > my_list[0]:
-            max_list = num
-            return max_list
+        if num > max_num:
+            max_num = num
+    return max_num
