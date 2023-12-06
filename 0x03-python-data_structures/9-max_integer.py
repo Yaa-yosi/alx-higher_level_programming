66fe49af2fc05bccd8b3a3268de915dba225ab43
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-12-06 11:20:43 +0000

    task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,4 +4,5 @@ def max_integer(my_list=[]):
         if not my_list:
             return None
         elif num > my_list[0]:
-            return num
+            max_list = num
+            return max_list
