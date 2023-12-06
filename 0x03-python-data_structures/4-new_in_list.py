4a7cd63f87c00f52331d58c185b1ba5b13137007
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-12-06 00:46:43 +0000

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,9 +2,9 @@
 def new_in_list(my_list, idx, element):
     tmp = my_list
     if idx < 0:
-        return tmp
+        return tmp.copy()
     elif idx >= len(my_list):
-        return tmp
+        return tmp.copy()
     else:
         tmp[idx] = element
         return tmp
