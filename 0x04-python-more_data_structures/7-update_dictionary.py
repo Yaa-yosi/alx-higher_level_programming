2ba229e24fcb0f500b5298b3fe15c859eb6ba34d
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-12-08 22:17:13 +0000

    task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary:
-        for key1 in a_dictionary:
-            if key1 == key:
-                a_dictionary[key] = value
-                return a_dictionary
-            elif key1 != key:
-                a_dictionary[key] = value
-                return a_dictionary
-
+    for key1 in a_dictionary:
+        if key1 == key:
+            a_dictionary[key] = value
+            return a_dictionary
+        elif key1 != key:
+            a_dictionary[key] = value
+            return a_dictionary
