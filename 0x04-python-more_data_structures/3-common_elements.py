c3937a88f41f9098a768be5b66310e9260fdfb27
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-12-08 12:42:44 +0000

    task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common_member = set_1.intersection(set_2)
-    for member in common_member:
-        return member
+    common_member = set_1 & set_2
+    return common_member
