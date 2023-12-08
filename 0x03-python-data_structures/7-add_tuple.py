fa222d8bc52117af44de83319858d872666a8d85
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-12-08 02:00:46 +0000

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    len_a, len_b = len(tuple_a), len(tuple_b)
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
     elem1 = (tuple_a[0] if len_a >= 1 else 0) + (tuple_b[0] if len_b >= 1 else 0)
     elem2 = (tuple_a[1] if len_a >= 2 else 0) + (tuple_b[1] if len_b >= 2 else 0)
     new_tuple = (elem1, elem2)
