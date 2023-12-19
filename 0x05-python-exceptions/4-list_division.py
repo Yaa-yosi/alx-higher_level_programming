e9abe5fdb1be9ef7c20f79bac54a084caadafa61
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-12-19 01:09:19 +0000

    task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,8 +8,10 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
             print("division by 0")
         except TypeError:
+            result = 0
             print("wrong type")
         except IndexError:
+            result = 0
             print("out of range")
         finally:
             pass
