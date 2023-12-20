28b3d35ddb987c585e57cdae673601b6d87507f0
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-12-20 05:32:46 +0000

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 class Square:
+    """ new class define as square"""
 
     def __init__(self, size):
+        """initializatio of new class with a new size"""
+
         self.__size = size
