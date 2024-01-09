13f2ce355cdadf1ae3f700f08b6ecadddc0874b8
Mercy Agbasah <agbasahmercy761@gmail.com>
2024-01-09 17:39:43 +0000

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,5 +2,5 @@
 
 def read_file(filename=""):
     with open("my_file_0.txt", encoding="UTF8") as new:
-        new1 = new.read()
-        print(new1)
+        for char in new: 
+            print(char, end="")
