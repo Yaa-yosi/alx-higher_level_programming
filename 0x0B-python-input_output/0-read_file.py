547db6f3278936fffd179452af4f6fc9650d766c
Mercy Agbasah <agbasahmercy761@gmail.com>
2024-01-09 18:01:49 +0000

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ function to read a file"""
 
+
 def read_file(filename=""):
     """reads a text file (UTF8) and prints it to stdout"""
     with open(filename, encoding="UTF8") as new:
         print(new.read(), end="")
+
