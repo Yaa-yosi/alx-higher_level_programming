89650a3a10febf8e3d729bd23e51e2715bc330b5
Mercy Agbasah <agbasahmercy761@gmail.com>
2024-04-04 14:22:18 +0000

    task-2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,12 +2,6 @@
 
 const arg = process.argv.length - 2;
 
-if (arg === 0){
-	console.log('No argument');
-}
-else if (arg === 1){
-	console.log('Argument found');
-}
-else{
-	console.log('Arguments found');
-}
+if (arg === 0) console.log('No argument');
+else if (arg === 1) console.log('Argument found');
+else console.log('Arguments found');
