f5b743d006be6e6cf11f8bfffc068f9ae0e79e5a
Mercy Agbasah <agbasahmercy761@gmail.com>
2024-04-04 16:07:43 +0000

    task-7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,5 +5,7 @@ let i;
 
 if (!arg) console.log('Missing number of occurrences');
 else{
-	for (i = 0; i < arg; i++) console.log('C is fun');
+	for (i = 0; i < arg; i++){
+		console.log('C is fun');
+	}
 }
